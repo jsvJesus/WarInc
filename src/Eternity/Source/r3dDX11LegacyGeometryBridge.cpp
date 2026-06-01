da4f807a9c97953eea9b7c384b3ac354b6829828
Jesus <es3n1n.eng@gmail.com>
2026-06-02 02:05:54 +0300

    add dx

--- src/Eternity/Source/r3dDX11LegacyGeometryBridge.cpp
+++ src/Eternity/Source/r3dDX11LegacyGeometryBridge.cpp
@@ -623,7 +623,7 @@ void r3dDX11LegacyGeometryBridge::LegacyDrawIndexedPrimitiveUP(
 	if(dx11IndexFormat == R3D_DX11_INDEX_UNKNOWN)
 		return;
 
-	g_r3dDX11Geometry.DrawIndexedPrimitiveUP(
+	g_r3dDX11Geometry.DrawIndexedPrimitiveUP( // ebana v rot
 		dx11PrimitiveType,
 		minVertexIndex,
 		numVertices,
